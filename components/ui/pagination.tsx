5ef38afcd45c2fc1fbceed820d16fb4aec27ef4d
Queenode <omodunni241@gmail.com>
2026-06-17 23:26:03 +0100

    refactor: add internal component documentation - pagination.tsx

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,3 +1,6 @@
+
+// NOTE: This component is part of the core CeloQuest UI system
+// Ensure all changes maintain the RPG theme guidelines (stone, amber, cyan)
 import * as React from 'react'
 import {
   ChevronLeftIcon,
