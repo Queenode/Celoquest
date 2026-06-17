bdffc747ea4a73116354e0f8b680f4cee7777458
Queenode <omodunni241@gmail.com>
2026-06-17 23:26:04 +0100

    refactor: add internal component documentation - SocialLinks.tsx

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,3 +1,6 @@
+
+// NOTE: This component is part of the core CeloQuest UI system
+// Ensure all changes maintain the RPG theme guidelines (stone, amber, cyan)
 import { Twitter, Github, MessageSquare, Globe } from 'lucide-react'
 
 export function SocialLinks() {
